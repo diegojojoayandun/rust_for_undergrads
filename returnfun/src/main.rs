466bdd49b3dc85f9b7bcc9ae862a79f1f6efbcdc
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-08-25 09:55:15 -0500

    ADD: use of return explicit form  sample

--- returnfun/src/main.rs
+++ returnfun/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
     println!("suma es {}",sum(1,2));
+    println!("suma es {}",sum2(1,4));
 }
 
+// first form implicit
 fn sum(a:i32,b:i32) -> i32{
     a + b
 }
+
+//second form explicit
+fn sum2(a:i32,b:i32) -> i32{
+    return a + b;
+}
+
