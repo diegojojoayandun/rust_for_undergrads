85f9a95a036c59b5b59deb513b44143418a2daa7
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-08-25 09:51:00 -0500

    ADD: use of return sample

---
+++ returnfun/target/debug/deps/returnfun.d
@@ -0,0 +1,5 @@
+E:\RUST\returnfun\target\debug\deps\returnfun.exe: src\main.rs
+
+E:\RUST\returnfun\target\debug\deps\returnfun.d: src\main.rs
+
+src\main.rs:
---
+++ returnfun/target/debug/returnfun.d
@@ -0,0 +1,1 @@
+E:\RUST\returnfun\target\debug\returnfun.exe: E:\RUST\returnfun\src\main.rs
