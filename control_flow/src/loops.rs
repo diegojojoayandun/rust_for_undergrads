b3a7444589b7bf28a79aa47e88a84f6ad848dbca
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-08-25 21:59:18 -0500

    ADD: use of loops

---
+++ control_flow/src/loops.rs
@@ -0,0 +1,17 @@
+//loop
+fn main(){
+    //infinite loop
+    loop{
+        println!("hello world");
+        break;
+
+        // si agregamos break solo lo ejecuta una vez
+    }
+
+    let mut a = 3;
+// whele evalua primerop la condición
+    while a  !=0 {
+        println!(" {} ", a);
+        a = a - 1;
+    }
+}
